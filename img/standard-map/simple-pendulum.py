8530b9db9156b9c2b8471c3429bbfdc0ea617d88
Alessandro Piazza <alepiazza@live.it>
2018-10-18 17:13:12 +0200

    Immagini pendolo

---
+++ img/standard-map/simple-pendulum.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def E(x, y):
+    return 0.5*(y**2) + np.cos(x)
+    
+n = 1000
+
+x_ = np.linspace(0, 2*np.pi, num=n)
+y_ = np.linspace(-2, 2, num=n)
+x, y = np.meshgrid(x_, y_)
+
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif', size=11)
+plt.rcParams['contour.negative_linestyle'] = 'solid'
+
+plt.contour(x, y, E(x, y),  60, colors='b', linewidths=0.3)
+plt.xlabel('$ x $')
+plt.ylabel('$ y $')
+plt.savefig('fase-pendolo.png', dpi=1200)
+plt.close()
